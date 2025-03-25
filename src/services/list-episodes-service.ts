a656b740f4a9340c7b8e55ecc95b8b9b573f8724
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-03-25 17:56:34 -0300

    creating respository and models

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,20 +1,8 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository";
 
 
 export const serviceListEpisodes =  async () => {
-    const data = [
-        {
-            podcastName: "Flow",
-            episode: "CBUM - Flow #319",
-            videoId: "pQSuQmUfS30&pp",
-            category: ["saúde", "bodybuilder", "esporte"]
-        },
-        {
-            podcastName: "Flow",
-            episode: "LEANDRO HASSUM - Flow #254",
-            videoId: "VsMWR1bfY1E&pp",
-            category: ["humor"]
-        },
-    ];
+    const data = await repositoryPodcast;
 
     return data;
 };
