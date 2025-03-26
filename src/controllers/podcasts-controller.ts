a2992db92589ab5a925415b605b79361e3a3569f
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-03-26 15:59:21 -0300

    creating routes

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -10,3 +10,14 @@ export const getlistEspiodes = async (
     res.writeHead(200,{'content-type': "application/json"});
     res.end(JSON.stringify(content));
 };
+
+export const getFilterEpisodes = async(
+    req: IncomingMessage,
+    res: ServerResponse,
+) => {
+    const content = await serviceListEpisodes('flow');
+
+    res.writeHead(200, {"Content-Type": "application/json" });
+    res.end(JSON.stringify(content));
+
+};
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import * as http from "http";
-import { getlistEspiodes } from "./controllers/podcasts-controller";
+import { getFilterEpisodes, getlistEspiodes } from "./controllers/podcasts-controller";
 
 const server = http.createServer( async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    if(req.method === "GET"){
+    if(req.method === "GET" && req.url === "/api/list") {
         await getlistEspiodes(req, res);
     }
-}
+
+    if(req.method === "GET" && req.url === "api/episode"){
+        await getFilterEpisodes(req, res);
+    }
+    }
 );
 
 const port = process.env.PORT;
